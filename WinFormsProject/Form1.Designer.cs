1c316f119ca18400187b2cb73be1e6c1f70e73e6
Дмитрий Рындаков <branzino@mail.ru>
2024-07-12 00:01:06 +0500

    Домашние Задание

--- WinFormsProject/Form1.Designer.cs
+++ WinFormsProject/Form1.Designer.cs
@@ -49,6 +49,7 @@
             saveDialog = new SaveFileDialog();
             btnOpen = new Button();
             btnSave = new Button();
+            btnRun = new Button();
             groupCheck.SuspendLayout();
             MenuTest.SuspendLayout();
             SuspendLayout();
@@ -56,9 +57,10 @@
             // btnTest
             // 
             btnTest.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            btnTest.Location = new Point(519, 453);
+            btnTest.Location = new Point(775, 604);
+            btnTest.Margin = new Padding(3, 4, 3, 4);
             btnTest.Name = "btnTest";
-            btnTest.Size = new Size(115, 37);
+            btnTest.Size = new Size(131, 49);
             btnTest.TabIndex = 0;
             btnTest.Text = "1";
             btnTest.UseVisualStyleBackColor = true;
@@ -67,9 +69,10 @@
             // BtnTest2
             // 
             BtnTest2.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
-            BtnTest2.Location = new Point(23, 467);
+            BtnTest2.Location = new Point(26, 622);
+            BtnTest2.Margin = new Padding(3, 4, 3, 4);
             BtnTest2.Name = "BtnTest2";
-            BtnTest2.Size = new Size(75, 23);
+            BtnTest2.Size = new Size(86, 31);
             BtnTest2.TabIndex = 1;
             BtnTest2.Text = "PushTo";
             BtnTest2.UseVisualStyleBackColor = true;
@@ -78,19 +81,20 @@
             // LblTest
             // 
             LblTest.AutoSize = true;
-            LblTest.Location = new Point(12, 43);
+            LblTest.Location = new Point(14, 57);
             LblTest.Name = "LblTest";
-            LblTest.Size = new Size(55, 15);
+            LblTest.Size = new Size(70, 20);
             LblTest.TabIndex = 2;
             LblTest.Text = "Подпись";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(97, 43);
+            textBox1.Location = new Point(111, 57);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
             textBox1.Multiline = true;
             textBox1.Name = "textBox1";
             textBox1.ScrollBars = ScrollBars.Both;
-            textBox1.Size = new Size(214, 356);
+            textBox1.Size = new Size(244, 473);
             textBox1.TabIndex = 3;
             // 
             // groupCheck
@@ -98,9 +102,11 @@
             groupCheck.Controls.Add(radioButton6);
             groupCheck.Controls.Add(radioButton5);
             groupCheck.Controls.Add(radioButton4);
-            groupCheck.Location = new Point(317, 66);
+            groupCheck.Location = new Point(362, 88);
+            groupCheck.Margin = new Padding(3, 4, 3, 4);
             groupCheck.Name = "groupCheck";
-            groupCheck.Size = new Size(161, 167);
+            groupCheck.Padding = new Padding(3, 4, 3, 4);
+            groupCheck.Size = new Size(184, 223);
             groupCheck.TabIndex = 4;
             groupCheck.TabStop = false;
             groupCheck.Text = "groupBox1";
@@ -108,9 +114,10 @@
             // radioButton6
             // 
             radioButton6.AutoSize = true;
-            radioButton6.Location = new Point(15, 76);
+            radioButton6.Location = new Point(17, 101);
+            radioButton6.Margin = new Padding(3, 4, 3, 4);
             radioButton6.Name = "radioButton6";
-            radioButton6.Size = new Size(94, 19);
+            radioButton6.Size = new Size(117, 24);
             radioButton6.TabIndex = 2;
             radioButton6.TabStop = true;
             radioButton6.Text = "radioButton6";
@@ -119,9 +126,10 @@
             // radioButton5
             // 
             radioButton5.AutoSize = true;
-            radioButton5.Location = new Point(15, 51);
+            radioButton5.Location = new Point(17, 68);
+            radioButton5.Margin = new Padding(3, 4, 3, 4);
             radioButton5.Name = "radioButton5";
-            radioButton5.Size = new Size(94, 19);
+            radioButton5.Size = new Size(117, 24);
             radioButton5.TabIndex = 1;
             radioButton5.TabStop = true;
             radioButton5.Text = "radioButton5";
@@ -130,9 +138,10 @@
             // radioButton4
             // 
             radioButton4.AutoSize = true;
-            radioButton4.Location = new Point(15, 28);
+            radioButton4.Location = new Point(17, 37);
+            radioButton4.Margin = new Padding(3, 4, 3, 4);
             radioButton4.Name = "radioButton4";
-            radioButton4.Size = new Size(94, 19);
+            radioButton4.Size = new Size(117, 24);
             radioButton4.TabIndex = 0;
             radioButton4.TabStop = true;
             radioButton4.Text = "radioButton4";
@@ -141,9 +150,10 @@
             // radioButton1
             // 
             radioButton1.AutoSize = true;
-            radioButton1.Location = new Point(517, 92);
+            radioButton1.Location = new Point(591, 123);
+            radioButton1.Margin = new Padding(3, 4, 3, 4);
             radioButton1.Name = "radioButton1";
-            radioButton1.Size = new Size(94, 19);
+            radioButton1.Size = new Size(117, 24);
             radioButton1.TabIndex = 2;
             radioButton1.TabStop = true;
             radioButton1.Text = "radioButton1";
@@ -152,9 +162,10 @@
             // radioButton2
             // 
             radioButton2.AutoSize = true;
-            radioButton2.Location = new Point(517, 117);
+            radioButton2.Location = new Point(591, 156);
+            radioButton2.Margin = new Padding(3, 4, 3, 4);
             radioButton2.Name = "radioButton2";
-            radioButton2.Size = new Size(94, 19);
+            radioButton2.Size = new Size(117, 24);
             radioButton2.TabIndex = 5;
             radioButton2.TabStop = true;
             radioButton2.Text = "radioButton2";
@@ -163,9 +174,10 @@
             // radioButton3
             // 
             radioButton3.AutoSize = true;
-            radioButton3.Location = new Point(517, 142);
+            radioButton3.Location = new Point(591, 189);
+            radioButton3.Margin = new Padding(3, 4, 3, 4);
             radioButton3.Name = "radioButton3";
-            radioButton3.Size = new Size(94, 19);
+            radioButton3.Size = new Size(117, 24);
             radioButton3.TabIndex = 6;
             radioButton3.TabStop = true;
             radioButton3.Text = "radioButton3";
@@ -174,9 +186,10 @@
             // checkBox3
             // 
             checkBox3.AutoSize = true;
-            checkBox3.Location = new Point(332, 41);
+            checkBox3.Location = new Point(379, 55);
+            checkBox3.Margin = new Padding(3, 4, 3, 4);
             checkBox3.Name = "checkBox3";
-            checkBox3.Size = new Size(83, 19);
+            checkBox3.Size = new Size(101, 24);
             checkBox3.TabIndex = 7;
             checkBox3.Text = "checkBox3";
             checkBox3.UseVisualStyleBackColor = true;
@@ -184,10 +197,12 @@
             // MenuTest
             // 
             MenuTest.Dock = DockStyle.None;
+            MenuTest.ImageScalingSize = new Size(20, 20);
             MenuTest.Items.AddRange(new ToolStripItem[] { менюToolStripMenuItem, видToolStripMenuItem });
             MenuTest.Location = new Point(0, 0);
             MenuTest.Name = "MenuTest";
-            MenuTest.Size = new Size(100, 24);
+            MenuTest.Padding = new Padding(7, 3, 0, 3);
+            MenuTest.Size = new Size(123, 30);
             MenuTest.TabIndex = 8;
             MenuTest.Text = "Файл";
             MenuTest.ItemClicked += MenuTest_ItemClicked;
@@ -198,7 +213,7 @@
             менюToolStripMenuItem.CheckState = CheckState.Checked;
             менюToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { меню1ToolStripMenuItem, меню2ToolStripMenuItem });
             менюToolStripMenuItem.Name = "менюToolStripMenuItem";
-            менюToolStripMenuItem.Size = new Size(53, 20);
+            менюToolStripMenuItem.Size = new Size(65, 24);
             менюToolStripMenuItem.Text = "Меню";
             // 
             // меню1ToolStripMenuItem
@@ -206,31 +221,38 @@
             меню1ToolStripMenuItem.Checked = true;
             меню1ToolStripMenuItem.CheckState = CheckState.Checked;
             меню1ToolStripMenuItem.Name = "меню1ToolStripMenuItem";
-            меню1ToolStripMenuItem.Size = new Size(114, 22);
+            меню1ToolStripMenuItem.Size = new Size(142, 26);
             меню1ToolStripMenuItem.Text = "Меню1";
             // 
             // меню2ToolStripMenuItem
             // 
             меню2ToolStripMenuItem.Name = "меню2ToolStripMenuItem";
-            меню2ToolStripMenuItem.Size = new Size(114, 22);
+            меню2ToolStripMenuItem.Size = new Size(142, 26);
             меню2ToolStripMenuItem.Text = "Меню2";
             // 
             // видToolStripMenuItem
             // 
             видToolStripMenuItem.Name = "видToolStripMenuItem";
-            видToolStripMenuItem.Size = new Size(39, 20);
+            видToolStripMenuItem.Size = new Size(49, 24);
             видToolStripMenuItem.Text = "Вид";
             // 
             // openDialog
             // 
             openDialog.DefaultExt = "txt";
             openDialog.FileName = "openFileDialog1";
+            openDialog.Filter = "Текстовые файлы(*.txt)|*.txt";
+            // 
+            // saveDialog
+            // 
+            saveDialog.DefaultExt = "txt";
+            saveDialog.Filter = "Текстовые файлы(*.txt)|*.txt";
             // 
             // btnOpen
             // 
-            btnOpen.Location = new Point(557, 217);
+            btnOpen.Location = new Point(637, 289);
+            btnOpen.Margin = new Padding(3, 4, 3, 4);
             btnOpen.Name = "btnOpen";
-            btnOpen.Size = new Size(75, 23);
+            btnOpen.Size = new Size(86, 31);
             btnOpen.TabIndex = 9;
             btnOpen.Text = "Open";
             btnOpen.UseVisualStyleBackColor = true;
@@ -238,19 +260,33 @@
             // 
             // btnSave
             // 
-            btnSave.Location = new Point(559, 246);
+            btnSave.Location = new Point(639, 328);
+            btnSave.Margin = new Padding(3, 4, 3, 4);
             btnSave.Name = "btnSave";
-            btnSave.Size = new Size(75, 23);
+            btnSave.Size = new Size(86, 31);
             btnSave.TabIndex = 10;
             btnSave.Text = "Save";
             btnSave.UseVisualStyleBackColor = true;
             btnSave.Click += btnSave_Click;
             // 
+            // btnRun
+            // 
+            btnRun.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            btnRun.Location = new Point(379, 613);
+            btnRun.Margin = new Padding(3, 4, 3, 4);
+            btnRun.Name = "btnRun";
+            btnRun.Size = new Size(145, 31);
+            btnRun.TabIndex = 11;
+            btnRun.Text = "Поймай";
+            btnRun.UseVisualStyleBackColor = true;
+            btnRun.MouseMove += btnRun_MouseMove;
+            // 
             // FormMain
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(646, 502);
+            ClientSize = new Size(920, 669);
+            Controls.Add(btnRun);
             Controls.Add(btnSave);
             Controls.Add(btnOpen);
             Controls.Add(checkBox3);
@@ -264,6 +300,7 @@
             Controls.Add(btnTest);
             Controls.Add(MenuTest);
             MainMenuStrip = MenuTest;
+            Margin = new Padding(3, 4, 3, 4);
             Name = "FormMain";
             Text = "Тестовое приложение";
             WindowState = FormWindowState.Maximized;
@@ -298,5 +335,6 @@
         private ToolStripMenuItem видToolStripMenuItem;
         private Button btnOpen;
         private Button btnSave;
+        private Button btnRun;
     }
 }
--- WinFormsProject/Form1.cs
+++ WinFormsProject/Form1.cs
@@ -40,26 +40,46 @@ namespace WinFormsProject
         {
 
         }
-
+        string fileName;
         private void btnSave_Click(object sender, EventArgs e)
         {
-            
+            if(saveDialog.ShowDialog() == DialogResult.Cancel)
+            { return; }
+            fileName = saveDialog.FileName;
+            System.IO.File.WriteAllText(fileName, textBox1.Text);
+            MessageBox.Show("Файл сохранен");
 
         }
 
-        string fileName;
+        //string fileName;
         private void btnOpen_Click(object sender, EventArgs e)
         {
-            if(openDialog.ShowDialog() == DialogResult.OK) { fileName = openDialog.FileName; }
-            textBox1.Text +=  fileName + Environment.NewLine;
+            if (openDialog.ShowDialog() == DialogResult.OK) { fileName = openDialog.FileName; }
+            textBox1.Text += fileName + Environment.NewLine;
             StreamReader sr = new StreamReader(fileName);
-            while(!sr.EndOfStream)
+            while (!sr.EndOfStream)
             {
                 textBox1.Text += sr.ReadLine() + Environment.NewLine;
 
             }
             sr.Close();
 
+        }
+
+        //Этот метод делает убегающую кнопку при нажатии
+        //private void btnRun_MouseClick(object sender, EventArgs e)
+        //{
+        //    Random r = new Random();
+        //    btnRun.Left = r.Next(0, this.Size.Width - btnRun.Width);
+        //    btnRun.Top = r.Next(0, this.Size.Height - btnRun.Height);
+        //}
+
+        //Этот метод делает убегающую кнопку при наведении на него курсора мыши
+        private void btnRun_MouseMove(object sender, MouseEventArgs e)
+        {
+            Random r = new Random();
+            btnRun.Left = r.Next(0, this.Size.Width - btnRun.Width);
+            btnRun.Top = r.Next(0, this.Size.Height - btnRun.Height);
         }
     }
 }
